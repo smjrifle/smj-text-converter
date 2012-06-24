4ee72707d0fa4d006717564b4028686361699533
smjrifle <rudd.mutd@gmail.com>
2012-06-24 09:00:06 +0545

    Initial

---
+++ Smj_Text_converter/src/smjrifetextconverter/SmjrifeTextConverterApp.java
@@ -0,0 +1,44 @@
+/*
+ * SmjrifeTextConverterApp.java
+ */
+
+package smjrifetextconverter;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ */
+public class SmjrifeTextConverterApp extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new SmjrifeTextConverterView(this));
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of SmjrifeTextConverterApp
+     */
+    public static SmjrifeTextConverterApp getApplication() {
+        return Application.getInstance(SmjrifeTextConverterApp.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        launch(SmjrifeTextConverterApp.class, args);
+    }
+}
